d365f808ab7ac7f3710ae42cb9b4ae3532aacaa8
Anukul Chandra <101039186+AnukulPr1me@users.noreply.github.com>
2024-04-21 23:09:57 +0530

    deleted the old sorting visualizer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import SortingVisualizer from './pages/SortingVisualizer/SortingVisualizer'
 import PathFinding from './pages/PathFinding'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
@@ -15,9 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path='/sorting' element={<SortingVisualizer />} />
         <Route path="/path-finding" element={<PathFinding />} />
- 
       </Routes>
       <Footer/>
     
